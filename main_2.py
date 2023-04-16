b6b108257c08be252ef20fbf05b460d47e79c033
etoilefilante974 <63518709+etoilefilante974@users.noreply.github.com>
2023-04-16 15:25:01 +0200

    Add files via upload

--- main_2.py
+++ main_2.py
@@ -4,8 +4,11 @@
 #import tk
 import threading 
 import tkinter as tk
+from tkinter import *
 from datetime import datetime
+#import msnp
 
+res=""
 def fairejoli():
     for i in range(10):
         print(i*"x")
@@ -20,15 +23,61 @@ def ecriretfichier():
         testetest=input("")
         filin.write(testetest)
 
-def ihm():
+def sq(res):
+    import sqlite3
+    connection = sqlite3.connect("C:/Users/jauze/Desktop/dbz.db")
+    i=1
+    
+    cursor=connection.cursor()
+    fg=(cursor.lastrowid,res,i)
+    cursor.execute('INSERT INTO t_nb VALUES(?,?,?)',fg)
+    connection.commit()
+
+def Lire():
+    import sqlite
+
+class affichage():
+    
+    def __init__(self):
+        def getEntry():
+            res = chef.get()
+            print("ceci est le texte tape", res)
+            sq(res)
+        racine=tk.Tk()
+        label=tk.Label(racine, text="Bienvenur sur l'IHM de Mathias")
+        label1=tk.Label(racine, text=datetime.now().time())
+        label2=tk.Label(racine, text=C)
+        label3=tk.Label(racine, text=D)
+        bouton=tk.Button(racine, text="fexit", command=racine.destroy)
+        chef = tk.Entry(racine,bg="white")
+        bouton1=tk.Button(racine, text="Envoi texte sur base de donnée", command=getEntry)
+        bouton2=tk.Button(racine, text="Lire Base de donné", command=Lire)
+        label.pack(pady=20)
+        label1.pack()
+        label2.pack()
+        label3.pack()
+        chef.pack()
+        bouton1.pack()
+        bouton.pack(pady=20)
+        bouton2.pack()
+        racine.geometry("500x500")
+        racine.mainloop()
+    
+
+def ihm():      
     racine=tk.Tk()
     label=tk.Label(racine, text="Bienvenur sur l'IHM de Mathias")
     label1=tk.Label(racine, text=datetime.now().time())
     label2=tk.Label(racine, text=C)
     bouton=tk.Button(racine, text="fexit", command=racine.destroy)
+    
+    chef = tk.Entry(racine,bg="white")
+    #bouton1=tk.Button(racine, text="change couleur", command=print(label2.get()))
     label.pack(pady=20)
     label1.pack()
     label2.pack()
+    chef.pack()
+    #bouton1.pack()
     bouton.pack(pady=20)
     racine.geometry("500x500")
     #racine.mainloop()
@@ -38,11 +87,15 @@ def ihm():
 def afficher():
     print("je suis la fonction main")
 if __name__=="__main__":
+    #msn=msnp.Session()
     print("rentrez le texte")
+    res=""
     C=input()
+    D="---"
     fairejoli()
-    g=threading.Thread(target=ihm())
-    g.start()
+    #g=threading.Thread(target=affichage())
+    #g.start()
+    app=affichage()
     ecriretfichier()
     print("mai,,,,")
     nom="alain"
